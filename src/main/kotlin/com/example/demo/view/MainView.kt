8054d3656d920667f0a0ea0f64f56bc49b72350c
kelmer <kelmer@gmail.com>
2020-04-22 08:35:04 +0200

    initial ui test

--- src/main/kotlin/com/example/demo/view/MainView.kt
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -1,12 +1,27 @@
 package com.example.demo.view
 
 import com.example.demo.app.Styles
+import javafx.geometry.Orientation
 import tornadofx.*
 
 class MainView : View("Hello TornadoFX") {
-    override val root = hbox {
+    override val root = vbox {
+        menubar {
+            menu {
+                label("File")
+            }
+        }
         label(title) {
             addClass(Styles.heading)
+        }
+        splitpane(orientation = Orientation.HORIZONTAL){
+            vbox {
+                label("Pane 1")
+            }
+            vbox {
+                label("Pane 2")
+                imageview("http://aventuraoriginal.com/media/covers/beneath_a_steel_sky_1143_UKtmkWY.jpg")
+            }
         }
     }
 }
